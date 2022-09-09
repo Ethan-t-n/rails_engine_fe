ee9f903d7b00564e2fb61d992b0f17ba28eb424b
Ethan Nguyen <etnguyen16@gmail.com>
2022-09-08 22:20:05 -0600

    add controllers

---
+++ app/controllers/merchants_controller.rb
@@ -0,0 +1,10 @@
+class MerchantsController < ApplicationController
+  def index
+    @merchants = MerchantFacade.list_merchants
+  end
+
+  def show
+    @merchant = MerchantFacade.show_merchant(params[:id])
+    @items = MerchantFacade.show_merchant_items(params[:id])
+  end
+end
